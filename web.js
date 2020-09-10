e783279ab3c7943d88727fd6722b71dab852ebe6
justice ndou <mobiusndou@gmail.com>
2020-09-10 17:44:59 +0200

    chat updated

--- web.js
+++ web.js
@@ -1,8 +1,17 @@
-'use strict';
-// app main requires
-const feathers = require("@feathersjs/feathers");
-const express = require('@feathersjs/express');
-const socketio = require('@feathersjs/socketio');
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
 
 // chat requires
 const chat_utils = require('./chat-utils');
@@ -18,38 +27,14 @@ const { response } = require("@feathersjs/express");
 const PORT = process.env.PORT || 5000;
 
 
-//Servicees
-// please try adding methods as services
-
-// initializing express and feathers
-const app = express(feathers());
-
-// adding the ability to parse json
-app.use(express.json());
-
-//config socket io in realtion APIs
-app.configure(socketio());
-
-//Enable rest services
-app.configure(express.rest());
-
-// register services
-
-// app.use('/chat', new ChatService());
-
-// new connections connect to stream
-app.on('connection', conn => app.channel('stream').join(conn));
-
-// publish to stream
-app.publish(data => app.channel('stream'));
 
 
 app.listen(PORT).on('listening', () => console.log(`Realtime server running on ${PORT}`));
 
 
-app.io.on("connection", socket => {
+io.on("connection", socket => {
     console.log('socket', socket);
-  chat_utils.connections.push(socket);
+    chat_utils.connections.push(socket);
 
   
   // 
